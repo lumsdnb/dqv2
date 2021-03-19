5fa769df608640705cccd22cdb3611ffeed10171
lums <jeremy@lums.io>
2021-03-19 01:14:48 +0100

    Add placeholder login modal

---
+++ src/Components/StartModal.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Verdict.css';
+
+const StartModal = (props) => {
+  return (
+    <>
+      <div className="judge-ruling">
+        <h2>Hey!</h2>
+        <p>please choose your role in this round</p>
+      </div>
+    </>
+  );
+};
+
+export default StartModal;
