35d3d730b1b57769a14ce8201a844e8ee04a7881
lums <jeremy@lums.io>
2021-03-31 23:02:02 +0200

    Production test

--- src/App.js
+++ src/App.js
@@ -80,7 +80,7 @@ const App = () => {
   const socketRef = useRef();
 
   useEffect(() => {
-    socketRef.current = io.connect(localENDPOINT);
+    socketRef.current = io.connect(productionENDPOINT);
     socketRef.current.on('your id', (id) => {
       setYourID(id);
     });
