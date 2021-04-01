4200a97e379079f070461eb5dacb0d7d5a8f1175
lums <jeremyondrey@gmail.com>
2021-04-01 21:10:20 +0200

    Good enough voting screen for now

--- src/Components/VotingModal.js
+++ src/Components/VotingModal.js
@@ -20,7 +20,7 @@ const VotingModal = (props) => {
   };
 
   const sendRuling = () => {
-    props.sendRuling(ruling);
+    props.handleRuling(ruling);
   };
 
   function selectVote(e) {
@@ -35,125 +35,139 @@ const VotingModal = (props) => {
     props.voteFor(yourWinner);
   };
 
-  const round1 = props.usedCards[0].map((c, i) => (
-    <Card
-      size='smol'
-      key={c.i}
-      claim={c.body}
-      index={i}
-      role={c.role}
-      type={c.type}
-      spectatorRating={c.spectatorRating}
-      judgeRating={c.judgeRating}
-    />
-  ));
+  const round1 = props.usedCards[0].map((c, i) => {
+    return (
+      <Card
+        size='smol'
+        key={c.i}
+        claim={c.body}
+        index={i}
+        role={c.role}
+        type={c.type}
+        spectatorRating={c.spectatorRating}
+        judgeRating={c.judgeRating}
+      />
+    );
+  });
 
-  const round2 = props.usedCards[1].map((c, i) => (
-    <Card
-      size='smol'
-      key={c.i}
-      claim={c.body}
-      index={i}
-      role={c.role}
-      type={c.type}
-      spectatorRating={c.spectatorRating}
-      judgeRating={c.judgeRating}
-    />
-  ));
+  const round2 = props.usedCards[1].map((c, i) => {
+    return (
+      <Card
+        size='smol'
+        key={c.i}
+        claim={c.body}
+        index={i}
+        role={c.role}
+        type={c.type}
+        spectatorRating={c.spectatorRating}
+        judgeRating={c.judgeRating}
+      />
+    );
+  });
 
-  const round3 = props.usedCards[2].map((c, i) => (
-    <Card
-      size='smol'
-      key={c.i}
-      claim={c.body}
-      index={i}
-      role={c.role}
-      type={c.type}
-      spectatorRating={c.spectatorRating}
-      judgeRating={c.judgeRating}
-    />
-  ));
-  const round4 = props.usedCards[3].map((c, i) => (
-    <Card
-      size='smol'
-      key={c.i}
-      claim={c.body}
-      index={i}
-      role={c.role}
-      type={c.type}
-      spectatorRating={c.spectatorRating}
-      judgeRating={c.judgeRating}
-    />
-  ));
+  const round3 = props.usedCards[2].map((c, i) => {
+    return (
+      <Card
+        size='smol'
+        key={c.i}
+        claim={c.body}
+        index={i}
+        role={c.role}
+        type={c.type}
+        spectatorRating={c.spectatorRating}
+        judgeRating={c.judgeRating}
+      />
+    );
+  });
+  const round4 = props.usedCards[3].map((c, i) => {
+    return (
+      <Card
+        size='smol'
+        key={c.i}
+        claim={c.body}
+        index={i}
+        role={c.role}
+        type={c.type}
+        spectatorRating={c.spectatorRating}
+        judgeRating={c.judgeRating}
+      />
+    );
+  });
 
   return (
     <>
       <div className='voting-modal'>
-        <div className='four-rows'>
-          <div className='scrolling-cards'>{round1}</div>
-          <div className='scrolling-cards'>{round2}</div>
-          <div className='scrolling-cards'>{round3}</div>
-          <div className='scrolling-cards'>{round4}</div>
-        </div>
-        <div className='center-column'>
-          <div className='neo-box-outward'>
-            <h4>
-              <sup>Thema:</sup>
-              {props.topic}
-            </h4>
-            <h2>Wer hat am besten argumentiert?</h2>
+        <div class='voting-grid-container'>
+          <div class='title-topic'>
+            <h4>topic:{props.topic}</h4>
           </div>
-          <div className='neo-box-inward'>
-            <div className='flex-row'>
-              <button
-                style={{
-                  border: yourWinner == 'aff' ? 'medium dashed green' : 'none',
-                }}
-                onClick={() => {
-                  selectVote('aff');
-                }}
-              >
-                <Player
-                  avi={props.game.affirmativeAvi}
-                  name={props.game.affirmativeName}
-                  role={'pro'}
-                />
-              </button>
-              <button
-                style={{
-                  border: yourWinner == 'tie' ? 'medium dashed green' : 'none',
-                }}
-                onClick={() => {
-                  selectVote('tie');
-                }}
-              >
-                <FaBlackTie className='big-icon' />
-              </button>
-              <button
-                style={{
-                  border: yourWinner == 'neg' ? 'medium dashed green' : 'none',
-                }}
-                onClick={() => {
-                  selectVote('neg');
-                }}
-              >
-                <Player
-                  avi={props.game.negativeAvi}
-                  name={props.game.negativName}
-                  role={'contra'}
-                />
-              </button>
-            </div>
-            <div className='neo-box-outward'>
-              {props.role == 'judge' ? (
-                <>
-                  <h4>dein Urteil:</h4>
-                  <textarea onChange={handleRuling} />
-                </>
-              ) : null}
-              {yourWinner != 'unset' ? (
-                <button onClick={sendVote}>abstimmen</button>
-              ) : null}
+          <div className='title-q'>
+            <h3>Wer hat am besten argumentiert?</h3>
+          </div>
+          <div className='round1-title vote-title'>Runde 1</div>
+          <div className='round2-title vote-title'>Runde 2</div>
+          <div className='round3-title vote-title'>Runde 3</div>
+          <div className='round4-title vote-title'>Runde 4</div>
+          <div className='round1 scroll-round'>{round1}</div>
+          <div className='round2 scroll-round'>{round2}</div>
+          <div className='round3 scroll-round'>{round3}</div>
+          <div className='round4 scroll-round'>{round4}</div>
+          <div className='voting'>
+            <div className='neo-box-inward'>
+              <div className='flex-row'>
+                <button
+                  className={
+                    yourWinner == 'aff' ? 'select-highlight' : 'select-buttons'
+                  }
+                  onClick={() => {
+                    selectVote('aff');
+                  }}
+                >
+                  <Player
+                    avi={props.game.affirmativeAvi}
+                    name={props.game.affirmativeName}
+                    role={'pro'}
+                  />
+                </button>
+                <button
+                  className={
+                    yourWinner == 'tie' ? 'select-highlight' : 'select-buttons'
+                  }
+                  onClick={() => {
+                    selectVote('tie');
+                  }}
+                >
+                  <FaBlackTie className='big-icon' />
+                </button>
+                <button
+                  className={
+                    yourWinner == 'neg' ? 'select-highlight' : 'select-buttons'
+                  }
+                  onClick={() => {
+                    selectVote('neg');
+                  }}
+                >
+                  <Player
+                    avi={props.game.negativeAvi}
+                    name={props.game.negativName}
+                    role={'contra'}
+                  />
+                </button>
+              </div>
+              <div className='neo-box-outward flex-row'>
+                {props.role == 'judge' ? (
+                  <>
+                    <h4>dein Urteil:</h4>
+                    <textarea
+                      className='judgement-textarea'
+                      onChange={handleRuling}
+                    />
+                  </>
+                ) : null}
+                {yourWinner != 'unset' ? (
+                  <button onClick={sendVote}>abstimmen</button>
+                ) : null}
+              </div>
             </div>
           </div>
         </div>
