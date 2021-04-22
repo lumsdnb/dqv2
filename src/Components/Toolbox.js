2b5fc83d37dc3f380d392b66c1f4f6208ac89a4e
lums <jeremyondrey@gmail.com>
2021-04-22 12:38:17 +0200

    Prettify, change btn text

--- src/Components/Toolbox.js
+++ src/Components/Toolbox.js
@@ -1,15 +1,15 @@
-import React, { useState } from "react";
-import BlockUi from "react-block-ui";
+import React, { useState } from 'react';
+import BlockUi from 'react-block-ui';
 
-import "react-block-ui/style.css";
+import 'react-block-ui/style.css';
 
-import "./Toolbox.css";
-import { GiBangingGavel } from "react-icons/gi";
+import './Toolbox.css';
+import { GiBangingGavel } from 'react-icons/gi';
 
 const Toolbox = (props) => {
-  const [yourArgument, setYourArgument] = useState("");
-  const [cardType, setCardType] = useState("argument");
-  const [cardSource, setCardSource] = useState("");
+  const [yourArgument, setYourArgument] = useState('');
+  const [cardType, setCardType] = useState('argument');
+  const [cardSource, setCardSource] = useState('');
 
   function handleArgument(e) {
     setYourArgument(e.target.value);
@@ -24,9 +24,9 @@ const Toolbox = (props) => {
   };
 
   function sendMessage(e) {
-    if (e === "") return;
-    if (yourArgument === "") return;
-    if (cardType === "fact") {
+    if (e === '') return;
+    if (yourArgument === '') return;
+    if (cardType === 'fact') {
       const messageObject = {
         body: yourArgument,
         role: props.role,
@@ -46,8 +46,8 @@ const Toolbox = (props) => {
       };
       props.sendMessage(messageObject);
     }
-    document.getElementById("toolbox-form").reset();
-    setYourArgument("");
+    document.getElementById('toolbox-form').reset();
+    setYourArgument('');
   }
 
   const spectatorView = () => {
@@ -65,12 +65,12 @@ const Toolbox = (props) => {
       tag='div'
       blocking={!props.canSend}
       message=''
-      style={{ borderRadius: "100vh" }}
+      style={{ borderRadius: '100vh' }}
     >
-      <div style={{ height: "100vh" }}>
-        {props.role === "spectator" ? spectatorView : null}
+      <div style={{ height: '100vh' }}>
+        {props.role === 'spectator' ? spectatorView : null}
 
-        {props.role === "judge" ? (
+        {props.role === 'judge' ? (
           <div classname='tools-judge'>
             <button className='gavel-btn' onClick={props.playGavel}>
               <GiBangingGavel />
@@ -79,22 +79,22 @@ const Toolbox = (props) => {
             <button onClick={props.nextRound}>Runde beenden</button>
           </div>
         ) : null}
-        {props.role === "affirmative" ||
-        props.role === "negative" ||
-        props.role === "debater" ? (
+        {props.role === 'affirmative' ||
+        props.role === 'negative' ||
+        props.role === 'debater' ? (
           <>
             <div class='select-buttons'>
               <button
-                className={cardType === "argument" ? "select-highlight" : null}
+                className={cardType === 'argument' ? 'select-highlight' : null}
                 type='button'
                 name='card_type'
                 value='argument'
                 onClick={handleCardType}
               >
-                Arg
+                Argument
               </button>
               <button
-                className={cardType === "fact" ? "select-highlight" : null}
+                className={cardType === 'fact' ? 'select-highlight' : null}
                 type='button'
                 name='card_type'
                 value='fact'
@@ -103,13 +103,13 @@ const Toolbox = (props) => {
                 Fakt
               </button>
               <button
-                className={cardType === "question" ? "select-highlight" : null}
+                className={cardType === 'question' ? 'select-highlight' : null}
                 type='button'
                 name='card_type'
                 value='question'
                 onClick={handleCardType}
               >
-                ???
+                Frage
               </button>
             </div>
           </>
@@ -121,7 +121,7 @@ const Toolbox = (props) => {
             onChange={handleArgument}
             maxLength='200'
           />
-          {cardType === "fact" ? (
+          {cardType === 'fact' ? (
             <textarea
               id='cardsource'
               className='source-field'
@@ -132,7 +132,7 @@ const Toolbox = (props) => {
           ) : null}
 
           <button type='button' className='form-btn' onClick={sendMessage}>
-            {props.role === "judge" ? "Urteil fällen" : "Karte spielen"}
+            {props.role === 'judge' ? 'Runde kommentieren' : 'Karte spielen'}
           </button>
         </form>
       </div>
